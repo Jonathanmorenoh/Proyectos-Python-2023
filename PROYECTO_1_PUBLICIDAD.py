c4a5f898ba1aa1da890d32dec9675b1fb0098b7f
Jonathan Moreno <jamh0207@gmail.com>
2023-02-22 10:56:15 -0400

    Pasando archivos del staging area al Repositorio Local

---
+++ PROYECTO_1_PUBLICIDAD.py
@@ -0,0 +1,9 @@
+#PROYECTO 1 - NOMBRE PUBLICITARIO PYTHON
+
+print('Creemos un nuevo slogan pulbicitario =)')
+
+ciudad = input('Hola, ingresa tu ciudad favorita: ')
+favorita = input('Ahora ingresa tu color favorito: ')
+
+print('El nombre ideal para tu cerveza sera: '+ciudad+' '+favorita)
+
